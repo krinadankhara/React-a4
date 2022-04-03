44b3a8dec8dc9f53627cc1523706267e715ecce1
Krina Dankhara <krinadankhara@Krinas-MacBook-Pro.local>
2022-04-03 15:09:37 -0400

    Implementing a4

---
+++ src/components/profile/login.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import * as service from "../../services/auth-service";
+
+export const Login = () => {
+  const [loginUser, setLoginUser] = useState({});
+  const navigate = useNavigate()
+  const login = () =>
+    service.login(loginUser).then((user) => navigate('/profile/mytuits')).catch(e => alert(e));
+  return (
+    <div>
+      <h1>Login</h1>
+      <input onChange={(e) => setLoginUser({...loginUser, username: e.target.value})}
+             placeholder="Username"
+             className="mb-2 form-control"/>
+      <input onChange={(e) => setLoginUser({...loginUser, password: e.target.value})}
+             type="password"
+             placeholder="Password"
+             className="mb-2 form-control"/>
+      <button onClick={login}>Login</button>
+    </div>
+  );
+};
---
+++ src/components/tuits/tuit-stats.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const TuitStats = ({tuit, likeTuit, dislikeTuit}) => {
+  return (
+    <div className="row mt-2">
+      <div className="col">
+        <i className="far fa-message me-1"/>
+        {tuit.stats && tuit.stats.replies}
+      </div>
+      <div className="col">
+        <i className="far fa-retweet me-1"/>
+        {tuit.stats && tuit.stats.retuits}
+      </div>
+      <div className="col">
+        <span onClick={() => likeTuit(tuit)}>
+          {
+            tuit.stats && tuit.stats.likes > 0 &&
+            <i className="far fa-thumbs-up fa-solid me-1" style={{color: 'blue'}}/>
+          }
+          {
+            tuit.stats && tuit.stats.likes <= 0 &&
+            <i className="far fa-thumbs-up me-1"/>
+          }
+          {tuit.stats && tuit.stats.likes}
+          </span>
+      </div>
+      <div className="col">
+        <i className="far fa-inbox-out"/>
+      </div>
+    </div>
+  );
+}
+
+export default TuitStats;
